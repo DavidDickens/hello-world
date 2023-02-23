0dfd5a9978ebbfa301c7059fae83fa81877cbe67
DavidDickens <gamrdave1@gmail.com>
2023-02-23 23:04:23 +0000

    adding functions

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-let userName = prompt("What do I call you?");
-console.log(userName);
 
-document.write("Welcome, " + userName + "Play me if you dare!");
+function yourName(){
+    let userName = prompt("What do I call you?");
+    console.log(userName);
+    document.write("Welcome, " + userName + "Play me if you dare!"); 
+    return userName;  
+}
 
-let doYouPlay = prompt("Do you want to play Chess? Type yes or no")
+function playChess(){
+let doYouPlay = prompt("Do you want to play me in a game of Chess?")
 console.log(doYouPlay)
 
 if(doYouPlay == 'yes'){
-    document.write("Click the link at the top to play!");
-    } else if (doYouPlay == 'no'){
-    document.write("I understand thinking is HARD...")
-}else { 
-    document.write("Hmm I'm not smart enough to know what you typed... How the turn tables")
+    console.log("Good Answer!! Click the link at the top to play!");
+} else {
+    console.log("Wrong answer but I understand thinking is HARD...");
+    doYouPlay = prompt("Do you want to play me in a game of Chess?");
+    if (doYouPlay == 'yes'){
+        console.log("Good Answer!! Click the link at the top to play!")
+    }
 }
+//} else if (doYouPlay == 'no'){
+ //   document.write("")
+//}else { 
+   // document.write("Hmm I'm not smart enough to know what you typed... How the turn tables")
+//}
+//}
+
 
